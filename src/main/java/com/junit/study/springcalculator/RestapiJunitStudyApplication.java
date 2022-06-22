5b6fb9fd197f8a28311b797abcafd5124340911e
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-23 00:42:15 +0900

    Initial commit

---
+++ src/main/java/com/junit/study/springcalculator/RestapiJunitStudyApplication.java
@@ -0,0 +1,13 @@
+package com.junit.study.springcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiJunitStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiJunitStudyApplication.class, args);
+    }
+
+}
